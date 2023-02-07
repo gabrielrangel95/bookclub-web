cac8bcf91beb0ac8b50a54efc7416918ec4fe824
Gabriel Rangel <gabriel@sevenapps.tech>
2023-02-07 16:11:08 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/no-unescaped-entities': 'off',
+    'multiline-ternary': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      node: { paths: ['src'] }
+    }
+  }
+}
