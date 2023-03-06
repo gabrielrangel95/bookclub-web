beb62cff36447ff2b3ffc341cfb0904a5b013778
Gabriel Rangel <gabriel@sevenapps.tech>
2023-03-06 15:16:15 -0500

    feat: detalhes do livro

--- src/modules/authenticated/screens/BookDetail/index.js
+++ src/modules/authenticated/screens/BookDetail/index.js
@@ -1,69 +1,64 @@
-import { Flex, Spinner } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 import { NavBar, Text, Button } from 'components'
+import { useParams } from 'react-router-dom'
 import { useQuery } from 'react-query'
 import { getBookDetail } from 'services/api/requests'
-import { useParams } from 'react-router-dom'
 
 export const BookDetailScreen = () => {
   const { id } = useParams()
-
-  const { isLoading, data } = useQuery(
-    ['booksById', id],
-    () => getBookDetail(id),
-    {
-      enabled: !!id
-    }
-  )
+  const { data } = useQuery(['bookDetail', id], () => getBookDetail(id), {
+    enabled: !!id
+  })
   console.log({ data })
 
   return (
     <Flex flexDir="column">
       <NavBar />
-      {isLoading && <Spinner />}
       <Flex
         flexDir="row"
-        alignItems="flex-start"
-        justifyContent="space-between"
         mt={['24px', '48px']}
         w="100%"
         paddingX={['24px', '48px', '80px', '112px']}
       >
         <Flex
-          w="238px"
-          h="358px"
-          borderRadius="12px"
-          backgroundImage={`url(${data?.data?.cover_url})`}
+          w={['238px']}
+          h={['358px']}
+          backgroundImage={`url(${data?.data?.book?.cover_url})`}
           backgroundSize="cover"
           backgroundPosition="center"
           backgroundRepeat="no-repeat"
+          borderRadius={['12px']}
         />
-        <Flex w="60%" mx="64px" flexDir="column">
-          <Text fontSize="24px" fontWeight="extrabold">
-            {data?.data?.name}
-          </Text>
-          <Text fontSize="16px" color="brand.greyDark">
-            {data?.data?.author?.name}
+
+        <Flex w="70%" mx="48px" flexDir="column">
+          <Text.ScreenTitle fontSize="24px">
+            {data?.data?.book?.name}
+          </Text.ScreenTitle>
+          <Text mt="6px" fontSize="16px" color="brand.greyDark">
+            {data?.data?.book?.author?.name}
           </Text>
 
-          <Text.ScreenTitle mt="12px">Informações</Text.ScreenTitle>
-          <Flex w="100%" flexDir="row" justifyContent="space-between">
+          <Text.ScreenTitle mt="16px">Informações</Text.ScreenTitle>
+          <Flex mt="4px" w="100%" flexDir="row" justifyContent="space-between">
             <Text fontSize="14px" color="brand.greyDark">
-              Categoria: {data?.data?.category?.name}
+              Categoria: {data?.data?.book?.category?.name}
             </Text>
             <Text fontSize="14px" color="brand.greyDark">
-              Páginas: {data?.data?.pages}
+              Páginas: {data?.data?.book?.pages}
             </Text>
             <Text fontSize="14px" color="brand.greyDark">
               Ano de lançamento:{' '}
-              {new Date(data?.data?.release_date).getFullYear()}
+              {new Date(data?.data?.book?.release_date).getFullYear()}
             </Text>
           </Flex>
-          <Text.ScreenTitle mt="12px">Sinopse</Text.ScreenTitle>
-          <Text fontSize="12px" color="brand.greyDark">
-            {data?.data?.synopsis}
+          <Text.ScreenTitle mt="16px">Sinopse</Text.ScreenTitle>
+          <Text mt="4px" fontSize="12px" color="brand.greyDark">
+            {data?.data?.book?.synopsis}
           </Text>
         </Flex>
-        <Button>Adicionar aos favoritos</Button>
+        <Flex>
+          <Button>Adicionar aos favoritos</Button>
+        </Flex>
       </Flex>
     </Flex>
   )
